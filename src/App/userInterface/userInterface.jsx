08d1d38242a8c04518d53cc8c484c781289f1879
luozhengjiafei <luozhengjiafei@gmail.com>
2020-08-21 00:47:13 -0700

    Update

--- src/App/userInterface/userInterface.jsx
+++ src/App/userInterface/userInterface.jsx
@@ -1,2 +1,15 @@
-import react from "react";
+import React from "react";
 import "./styles.css";
+
+function UserInterface() {
+
+    return(
+        <div>
+        <h1>Please enter the city name:</h1>
+        <label for="cityName">City:</label>
+        <input type="text" id="cityName" name="cityName"/><br/><br/>
+        <input type="submit" value="Submit"/>
+        </div>
+    );
+}
+export default UserInterface;
--- src/App/weatherUrl/weatherApi.jsx
+++ src/App/weatherUrl/weatherApi.jsx
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 import "./styles.css";
 
 const weather_url = "https://api.openweathermap.org/data/2.5/weather?id=6167865&appid={yourkey}";
